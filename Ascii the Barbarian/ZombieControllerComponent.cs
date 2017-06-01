8ec4c053f124b65975ca336d4c090d71d2d67f14
José Miguel Alvarez <jmalvarez@miuandes.cl>
2017-06-01 16:54:30 -0400

    blaa

--- Ascii the Barbarian/ZombieControllerComponent.cs
+++ Ascii the Barbarian/ZombieControllerComponent.cs
@@ -10,15 +10,27 @@ namespace Ascii_the_Barbarian
     {
         private int v;
         private int index;
+        private GraphAlgorithmSolver solver;
+        private List<Tuple<int, int>> path; 
 
-        public ZombieControllerComponent()
+        public ZombieControllerComponent(Level level)
         {
+            this.solver = new GraphAlgorithmSolver(new Graph(level));
             this.v = 1;
             this.index = 0;
+            this.path = new List<Tuple<int, int>>();
         }
         public void Update(GameObject gameObject, movement command, List<GameObject> gameObjects)
         {
-            
+            if (index % 4 == 0)
+            {
+                path = solver.AAlgorithmSolver( , gameObject);
+            }
+            else
+            {
+
+            }
+            index++;
         }
     }
 }
