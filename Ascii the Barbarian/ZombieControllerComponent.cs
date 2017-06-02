a7e0ee13f637af6068c9a5dcaeea4e6d40fa31dd
Juande Valenzuela <jdvalenzuela@miuandes.cl>
2017-06-01 23:29:33 -0400

    - Zombie movement

--- Ascii the Barbarian/ZombieControllerComponent.cs
+++ Ascii the Barbarian/ZombieControllerComponent.cs
@@ -29,8 +29,7 @@ namespace Ascii_the_Barbarian
                     {
                         int x = 0;
                         path = solver.AAlgorithmSolver(go, gameObject);
-                        Console.WriteLine("Largo");
-                        Console.WriteLine(path.Count);
+
                         foreach (Tuple<int, int> path_t in path){
                             Console.WriteLine(path_t);
                             Console.WriteLine(x);
